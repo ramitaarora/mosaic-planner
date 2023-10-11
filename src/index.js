36ad762db7cf92a13ce3298796f8e98c63fec3c2
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-11 16:52:56 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+    return (
+        <div id="app">test</div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
