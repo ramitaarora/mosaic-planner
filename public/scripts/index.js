433356033a9c853e97009c85e4a20f8c6623d313
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-11 18:39:56 -0700

    basic outline

---
+++ public/scripts/index.js
@@ -0,0 +1,26 @@
+console.log('Script connected to HTML');
+
+// // Today's Date and Time
+// var dayjs = require("dayjs");
+// import {dayjs} from '/dayjs';
+
+// var currentDay = document.querySelector('#current-day');
+// var currentTime = document.querySelector('#current-time');
+// var today = dayjs().format('dddd, MMMM D');
+// var time = Number(dayjs().format('HH'));
+
+// function setOrdinal() {
+//     if (today.slice(-1) == 1) {
+//         currentDay.append('st');
+//     } else if (today.slice(-1) == 2) {
+//         currentDay.append('nd');
+//     } else if (today.slice(-1) == 3) {
+//         currentDay.append('rd');
+//     } else {
+//         currentDay.append('th');
+//     }
+// }
+
+// setOrdinal();
+// currentDay.innerHTML = today;
+// currentTime.innerHTML = time;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'))
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
   });
