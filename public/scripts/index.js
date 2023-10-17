17ed78c2ca8591b718e83a3e0c6ce0bb22ba3ef8
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-17 10:21:25 -0700

    dayjs in header

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,22 @@
-console.log('Script connected to HTML');
+// Today's Date and Time
 
-// // Today's Date and Time
-// var dayjs = require("dayjs");
-// import {dayjs} from '/dayjs';
+var currentDay = document.querySelector('#current-day');
+var currentTime = document.querySelector('#current-time');
+var today = dayjs().format('dddd, MMMM D');
+var time = Number(dayjs().format('HH:MM A'));
 
-// var currentDay = document.querySelector('#current-day');
-// var currentTime = document.querySelector('#current-time');
-// var today = dayjs().format('dddd, MMMM D');
-// var time = Number(dayjs().format('HH'));
+function setOrdinal() {
+    if (today.slice(-1) == 1) {
+        currentDay.append('st');
+    } else if (today.slice(-1) == 2) {
+        currentDay.append('nd');
+    } else if (today.slice(-1) == 3) {
+        currentDay.append('rd');
+    } else {
+        currentDay.append('th');
+    }
+} 
 
-// function setOrdinal() {
-//     if (today.slice(-1) == 1) {
-//         currentDay.append('st');
-//     } else if (today.slice(-1) == 2) {
-//         currentDay.append('nd');
-//     } else if (today.slice(-1) == 3) {
-//         currentDay.append('rd');
-//     } else {
-//         currentDay.append('th');
-//     }
-// }
-
-// setOrdinal();
-// currentDay.innerHTML = today;
-// currentTime.innerHTML = time;
+currentDay.innerHTML = today;
+currentTime.innerHTML = time;
+setOrdinal();
