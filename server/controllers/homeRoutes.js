488f84a04aa50e1d86bc7eb08a687ac704491a7b
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-12-12 20:52:03 -0800

    changed server code

--- server/controllers/homeRoutes.js
+++ server/controllers/homeRoutes.js
@@ -1,104 +1,105 @@
 const router = require('express').Router();
-const { Posts, User, Comments } = require('../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    const blogPostData = await Posts.findAll({
-        include: [{model: User}],
-    });
+    res.json({message:'Hello!'})
+    // const blogPostData = await Posts.findAll({
+    //     include: [{model: User}],
+    // });
 
-    const blogPosts = blogPostData.map(post => post.get({plain:true}));
-    // console.log(blogPosts);
+    // const blogPosts = blogPostData.map(post => post.get({plain:true}));
+    // // console.log(blogPosts);
 
-    res.render('homepage', {
-        logged_in: req.session.logged_in,
-        blogPosts,
-    });
+    // res.render('homepage', {
+    //     logged_in: req.session.logged_in,
+    //     blogPosts,
+    // });
 });
 
-router.get('/login', (req, res) => {
-    if (!req.session.logged_in) {
-        res.render('login');
-    } else {
-        res.redirect('/', {logged_in: req.session.logged_in});
-    }
-});
+// router.get('/login', (req, res) => {
+//     if (!req.session.logged_in) {
+//         res.render('login');
+//     } else {
+//         res.redirect('/', {logged_in: req.session.logged_in});
+//     }
+// });
 
-router.get('/signup', (req, res) => {
-    if (!req.session.logged_in) {
-        res.render('signup');
-    } else {
-        res.redirect('/', {logged_in: req.session.logged_in});
-    }
-});
+// router.get('/signup', (req, res) => {
+//     if (!req.session.logged_in) {
+//         res.render('signup');
+//     } else {
+//         res.redirect('/', {logged_in: req.session.logged_in});
+//     }
+// });
 
-router.get('/dashboard', async (req, res) => {
-    if (!req.session.logged_in) {
-        res.render('login');
-    } else {
-        const blogPostData = await Posts.findAll({
-            include: [{model: User}],
-            where: {
-                author_id: req.session.user_id
-            }
-        });
+// router.get('/dashboard', async (req, res) => {
+//     if (!req.session.logged_in) {
+//         res.render('login');
+//     } else {
+//         const blogPostData = await Posts.findAll({
+//             include: [{model: User}],
+//             where: {
+//                 author_id: req.session.user_id
+//             }
+//         });
     
-        const blogPosts = blogPostData.map(post => post.get({plain:true}));
-        console.log(blogPosts);
-        res.render('dashboard', {
-            logged_in: req.session.logged_in,
-            blogPosts,
-        });
-    }
-});
+//         const blogPosts = blogPostData.map(post => post.get({plain:true}));
+//         console.log(blogPosts);
+//         res.render('dashboard', {
+//             logged_in: req.session.logged_in,
+//             blogPosts,
+//         });
+//     }
+// });
 
-router.get('/post/:id', async (req, res) => {
-    try {
-        const blogPostData = await Posts.findOne( { where: { id: req.params.id }, 
-        include: [{model:User}]
-        });
-        // console.log(blogPostData);
-        const blogPost = blogPostData.get({plain: true});
-        // console.log (blogPost);
+// router.get('/post/:id', async (req, res) => {
+//     try {
+//         const blogPostData = await Posts.findOne( { where: { id: req.params.id }, 
+//         include: [{model:User}]
+//         });
+//         // console.log(blogPostData);
+//         const blogPost = blogPostData.get({plain: true});
+//         // console.log (blogPost);
 
-        const commentData = await Comments.findAll( { where: { post_id: req.params.id},
-            include: [{model: User}]
-        })
-        const comments = commentData.map((comment => comment.get({plain:true})));
-        // console.log(comments);
+//         const commentData = await Comments.findAll( { where: { post_id: req.params.id},
+//             include: [{model: User}]
+//         })
+//         const comments = commentData.map((comment => comment.get({plain:true})));
+//         // console.log(comments);
 
-        res.render('post', {
-            blogPost,
-            comments,
-            logged_in: req.session.logged_in,
-        });
+//         res.render('post', {
+//             blogPost,
+//             comments,
+//             logged_in: req.session.logged_in,
+//         });
 
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
-router.get('/edit/:id', withAuth, async (req, res) => {
-    try {
-        const blogPostData = await Posts.findOne( { where: { id: req.params.id }, 
-        include: [{model:User}]
-        });
-        // console.log(blogPostData);
-        const blogPost = blogPostData.get({plain: true});
-        // console.log(blogPost);
+// router.get('/edit/:id', withAuth, async (req, res) => {
+//     try {
+//         const blogPostData = await Posts.findOne( { where: { id: req.params.id }, 
+//         include: [{model:User}]
+//         });
+//         // console.log(blogPostData);
+//         const blogPost = blogPostData.get({plain: true});
+//         // console.log(blogPost);
 
-        res.render('editPost', {
-            blogPost,
-            logged_in: req.session.logged_in,
-        });
+//         res.render('editPost', {
+//             blogPost,
+//             logged_in: req.session.logged_in,
+//         });
 
-    } catch (err) {
-        res.status(400).json(err);
-    }
-})
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// })
 
-router.get('/newPost', withAuth, (req, res) => {
-    res.render('newPost', { logged_in: req.session.logged_in} );
-})
+// router.get('/newPost', withAuth, (req, res) => {
+//     res.render('newPost', { logged_in: req.session.logged_in} );
+// })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const cors = require('cors');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,14 +29,15 @@ const sess = {
 };
 
 app.use(session(sess));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../client/dist'))); 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.use(routes);
