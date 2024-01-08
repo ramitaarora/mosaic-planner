90102c646f4fb948f5d61d358090e019b0f43f2b
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-08 11:21:11 -0800

    added google calendar api packages

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from 'react';
+import { gapi } from "gapi-script";
+
 export default function Schedule() {
     return (
         <div id="schedule">
             <h2>Schedule</h2>
+            
         </div>
     )
 }
