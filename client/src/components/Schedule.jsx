7fa531cd78437c40b0d3979dcba98902ab91afc2
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-09 10:25:16 -0800

    restarting api code

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,10 +1,6 @@
 import { useState, useEffect } from 'react';
-import { gapi } from "gapi-script";
 
 export default function Schedule() {
-    const calendarID = process.env.REACT_APP_CALENDAR_ID;
-    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
-    const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
     
     return (
         <div id="schedule">
