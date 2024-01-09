c2fae902bf30ef46204b8830b95f0874446c7c6d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-09 14:39:16 -0800

    api progress

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -5,7 +5,6 @@ export default function Calendar() {
     return (
         <div id="calendar" className="card">
         <div id="card-header"><h2>Calendar</h2></div>
-            
         </div>
     )
 }
