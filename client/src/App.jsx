8de13410916e542dd1a198bdee12cb8ea8ca5191
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-09 20:27:49 -0800

    react router, not working yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,6 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import Header from './components/Header.jsx';
-import YearlyGoals from './components/YearlyGoals.jsx';
-import MonthlyGoals from './components/MonthlyGoals.jsx';
-import WeeklyGoals from './components/WeeklyGoals.jsx';
-import DailyChecks from './components/DailyChecks.jsx';
-import Notes from './components/Notes.jsx';
-import Schedule from './components/Schedule.jsx';
-import CalendarCard from './components/CalendarCard.jsx';
-import CalendarClick from './components/CalendarClick.jsx';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  const [yearGoals, setYearGoals] = useState([]);
-  const [monthGoals, setMonthGoals] = useState([]);
-  const [weekGoals, setWeekGoals] = useState([]);
-  const [notes, setNotes] = useState([]);
-  const [checks, setChecks] = useState([]);
-  const [inputValue, setInputValue] = useState('');
- 
-  useEffect(() => {
-    fetch('/api/data/allGoals')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json(); // or response.text() for text data
-      })
-      .then((data) => {
-        // console.log(data);
-        setYearGoals(data.yearlyGoals.map(goal => goal));
-        setMonthGoals(data.monthlyGoals.map(goal => goal));
-        setWeekGoals(data.weeklyGoals.map(goal => goal));
-        setNotes(data.notes.map(note => note));
-        setChecks(data.dailyChecks.map(check => check.daily_check));
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <Header />
-
-      <main>
-        <section id="left">
-          <YearlyGoals yearGoals={yearGoals} setYearGoals={setYearGoals} inputValue={inputValue} setInputValue={setInputValue} />
-          <MonthlyGoals monthGoals={monthGoals} setMonthGoals={setMonthGoals} inputValue={inputValue} setInputValue={setInputValue} />
-          <WeeklyGoals weekGoals={weekGoals} setWeekGoals={setWeekGoals} inputValue={inputValue} setInputValue={setInputValue} />
-        </section>
-
-        <section id="middle">
-          <Schedule />
-          <DailyChecks checks={checks} setChecks={setChecks} />
-        </section>
-
-        <section id="right">
-          <Notes notes={notes} setNotes={setNotes} inputValue={inputValue} setInputValue={setInputValue} />
-          <CalendarCard />
-          <CalendarClick />
-        </section>
-      </main>
-
-    </div>
-  )
+  <Outlet />
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx'
+import Dashboard from './pages/Dashboard.jsx';
+import Landing from './pages/Landing.jsx';
+import Error from './pages/Error.jsx';
+import Account from './pages/Account.jsx';
+import ProfileSetup from './pages/ProfileSetup.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} fallbackElement={<Landing />}/>
+  </React.StrictMode>
+);
