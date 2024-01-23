b01e472bf80affef3da35c611f85e272c7e40d70
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-01-23 09:35:44 -0800

    goals modal functional

--- client/src/components/GoalsForm.jsx
+++ client/src/components/GoalsForm.jsx
@@ -7,43 +7,33 @@ export default function GoalsForm({ visibility, setVisibility }) {
         setVisibility('form-hidden');
     }
 
-    const submitNewGoal = async (event) => {
+    const saveGoal = async (event) => {
         event.preventDefault();
-        const newGoalValue = event.target[0].value;
+        const formID = event.target.id;
+        let yearlyGoal = event.target[0].value;
+        let monthlyGoal = event.target[1].value;
+        let weeklyGoal = event.target[2].value;
 
-        if (newGoalValue.length) {
-            const response = await fetch('/api/data/add', {
-                method: 'POST',
-                body: JSON.stringify({
-                    goal: newGoalValue,
-                    type: 'Goal'
-                }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-    
-            if (response.ok) {
-                // console.log(response.statusText);
-                fetch('/api/data/allData')
-                    .then((response) => {
-                        if (!response.ok) {
-                            throw new Error(`HTTP error! Status: ${response.status}`);
-                        }
-                        return response.json(); // or response.text() for text data
-                    })
-                    .then((data) => {
-                        // console.log(data);
-                        setGoals(data.goals.filter(goal => goal.goal_type === goalType));
-                        document.getElementById('add-goal').setAttribute('class', 'form-hidden');
-                        document.getElementById('cancel-goal-button').setAttribute('class', 'form-hidden');
-                        document.getElementById('add-goal-button').setAttribute('class', 'form-visible');
-                        setInputValue('')
-                    })
-                    .catch((error) => {
-                        console.error('Error fetching data:', error);
-                    });
-            } else {
-                alert(response.statusText);
-            }
+        const response = await fetch('/api/data/add', {
+            method: 'POST',
+            body: JSON.stringify({
+                type: 'Goal',
+                yearlyGoal: yearlyGoal,
+                monthlyGoal: monthlyGoal,
+                weeklyGoal: weeklyGoal,
+                goal_type: 'Goals',
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // console.log(response.statusText);
+            alert('Goal saved!')
+            document.getElementById(formID).reset();
+            closeModal();
+            location.reload();
+        } else {
+            alert(response.statusText);
         }
     }
 
@@ -54,7 +44,28 @@ export default function GoalsForm({ visibility, setVisibility }) {
                 <img src="./svgs/exit.svg" alt="exit" onClick={closeModal}/>
 
                     <div id="goals-modal">
-                    
+                        <form id="save-goal" onSubmit={saveGoal}>
+                            <p>Yearly resolutions break down into monthly goals, which can be further broken down into weekly goals.</p>
+                            <div id="form-input">
+                                <label htmlFor='yearly'>Yearly Goal</label>
+                                <input type="text" name="yearly" placeholder="Type your yearly goal here..." required />
+                            </div>
+
+                            <div id="form-input">
+                                <label htmlFor='monthly'>Monthly Goal</label>
+                                <input type="text" name="monthly" placeholder="Type your monthly goal here..." required />
+                            </div>
+
+                            <div id="form-input">
+                                <label htmlFor='weekly'>Weekly Goal</label>
+                                <input type="text" name="weekly" placeholder="Type your weekly goal here..." required />
+                            </div>
+
+                            <div id="form-submit-buttons">
+                                <input type="submit" value="Save" />
+                                <input type="reset" value="Reset" />
+                            </div>
+                        </form>
                     </div>
 
                 </div>
