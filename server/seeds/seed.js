83df10c0811619248a2631ed0f12788fafc4c9a1
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-03-18 09:25:31 -0700

    sequelize

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -18,54 +18,53 @@ const todaysDate = `${year}-${month}-${day}`;
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    const user = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-    // console.log("ID: " + user[0].dataValues.id);
+    // const user = await User.bulkCreate(userData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // });
 
-    const events = await Events.bulkCreate(eventsData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const events = await Events.bulkCreate(eventsData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
-    const dailyChecks = await DailyChecks.bulkCreate(dailyChecksData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const dailyChecks = await DailyChecks.bulkCreate(dailyChecksData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
-    const notes = await Notes.bulkCreate(notesData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const notes = await Notes.bulkCreate(notesData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
-    const parentGoals = await Goals.bulkCreate(parentGoalsData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const parentGoals = await Goals.bulkCreate(parentGoalsData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
-    const childGoals = await Goals.bulkCreate(childGoalsData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const childGoals = await Goals.bulkCreate(childGoalsData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
-    for (let i = 0; i < dailyChecksData.length; i++) {
-        const dailyChecksHistory = await DailyChecksHistory.create({
-            daily_check: dailyChecksData[i].daily_check,
-            user_id: dailyChecksData[i].user_id,
-            parent_id: dailyChecksData[i].id,
-            date: todaysDate,
-            completed: false,
-        }, {
-            individualHooks: true,
-            returning: true,
-        })
-    }
+    // for (let i = 0; i < dailyChecksData.length; i++) {
+    //     const dailyChecksHistory = await DailyChecksHistory.create({
+    //         daily_check: dailyChecksData[i].daily_check,
+    //         user_id: dailyChecksData[i].user_id,
+    //         parent_id: dailyChecksData[i].id,
+    //         date: todaysDate,
+    //         completed: false,
+    //     }, {
+    //         individualHooks: true,
+    //         returning: true,
+    //     })
+    // }
 
-    const tasks = await Tasks.bulkCreate(taskData, {
-        individualHooks: true,
-        returning: true,
-    })
+    // const tasks = await Tasks.bulkCreate(taskData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
 
     process.exit(0);
 };
