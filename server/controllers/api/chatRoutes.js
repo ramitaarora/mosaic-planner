67bc9fbfd58e6068e85f975557f22038ac8d2d21
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-09 10:10:55 -0700

    tuned goals for ai suggestions

--- server/controllers/api/chatRoutes.js
+++ server/controllers/api/chatRoutes.js
@@ -1,30 +1,47 @@
 const router = require('express').Router();
 const OpenAI = require('openai');
 const openai = new OpenAI();
+const { User, Goals, Notes, DailyChecks, DailyChecksHistory, Events, Tasks } = require('../../models');
 
-const goalTypes = ["Travel", "Reading", "Fitness", "Work", "Love Life", "Family", "Friendships", "Money", "Social Clubs"]
+const goalTypes = ["Travel", "Reading", "Fitness", "Work", "Love Life", "Family", "Friendships", "Money", "Clubs", "Career", "Saving", "Classes", "Hobbies", "Health"]
+let existingGoals = [];
 
 router.get('/goals', async (req, res) => {
   try {
-    let suggestion = goalTypes[Math.floor(Math.random() * goalTypes.length)];
+    const goalsData = await Goals.findAll({ where: { user_id: req.session.user_id } });
+    const goals = goalsData.map(goal => goal.get({ plain: true }));
+    
+    for (let i = 0; i < goals.length; i++) {
+      existingGoals.push(goals[i].goal);
+    }
+    
+    try {
+      let suggestion = goalTypes[Math.floor(Math.random() * goalTypes.length)];
 
-    const suggestionsData = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant designed to output JSON.",
-        },
-        { role: "user", content: "Can you give me a new yearly goal that breaks down into a monthly goal that breaks down into a weekly goal? Type of goal: " + suggestion + ". Only suggest one set of goals. Please format three short phrases in an array called 'goalSuggestions', in order of: yearly, monthly, weekly. Please only have the goal content in each array element, without 'yearly', 'monthly', or 'weekly' in the element." },
-      ],
-      model: "gpt-3.5-turbo-0125",
-    });
+      const suggestionsData = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant designed to output JSON.",
+          },
+          { role: "user", content: "Can you give me a new yearly goal that breaks down into a monthly goal that breaks down into a weekly goal? Type of goal: " + suggestion + ". Only suggest one set of goals. Please do not suggest any of these goals: " + existingGoals + ". Please format three short phrases in an array called 'goalSuggestions', in order of: yearly, monthly, weekly. Please only have the goal content in each array element, without 'yearly', 'monthly', or 'weekly' in the element." },
+        ],
+        model: "gpt-3.5-turbo-0125",
+      });
 
-    // console.log(suggestionsData.choices[0].message.content);
-    res.status(200).json(suggestionsData.choices[0].message.content);
+      // console.log(suggestionsData.choices[0].message.content);
+      res.status(200).json(suggestionsData.choices[0].message.content);
+    } catch (err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
+
   } catch (err) {
-    res.status(400).json(err);
-    console.log(err);
+      res.status(400).json(err);
+      console.log(err);
   }
+
+
 });
 
 router.get('/notes', async (req, res) => {
