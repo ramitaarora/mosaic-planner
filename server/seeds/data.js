880d9ad7a2bcfbe45ac680202a386850204336ca
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-10 15:13:51 -0700

    data

--- server/seeds/data.js
+++ server/seeds/data.js
@@ -7,25 +7,25 @@ const seeData = async () => {
         const users = userData.map(user => user.get({ plain: true }));
         console.log(users);
 
-        const checksData = await DailyChecks.findAll();
-        const checks = checksData.map(check => check.get({ plain: true }));
-        console.log(checks);
+        // const checksData = await DailyChecks.findAll();
+        // const checks = checksData.map(check => check.get({ plain: true }));
+        // console.log(checks);
 
-        const eventsData = await Events.findAll();
-        const events = eventsData.map(event => event.get({ plain: true }));
-        console.log(events);
+        // const eventsData = await Events.findAll();
+        // const events = eventsData.map(event => event.get({ plain: true }));
+        // console.log(events);
 
-        const goalsData = await Goals.findAll();
-        const goals = goalsData.map(goal => goal.get({ plain: true }));
-        console.log(goals);
+        // const goalsData = await Goals.findAll();
+        // const goals = goalsData.map(goal => goal.get({ plain: true }));
+        // console.log(goals);
 
-        const notesData = await Notes.findAll();
-        const notes = notesData.map(note => note.get({ plain: true }));
-        console.log(notes);
+        // const notesData = await Notes.findAll();
+        // const notes = notesData.map(note => note.get({ plain: true }));
+        // console.log(notes);
 
-        const tasksData = await Tasks.findAll();
-        const tasks = tasksData.map(task => task.get({ plain: true }));
-        console.log(tasks);
+        // const tasksData = await Tasks.findAll();
+        // const tasks = tasksData.map(task => task.get({ plain: true }));
+        // console.log(tasks);
 
     } catch (error) {
         console.log(error);
