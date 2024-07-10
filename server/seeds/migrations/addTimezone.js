fff734ccf2f3f4c63f674c202da2f962f0568bc9
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-10 14:46:53 -0700

    console logging

--- server/seeds/migrations/addTimezone.js
+++ server/seeds/migrations/addTimezone.js
@@ -1,17 +1,24 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/connection');
 const queryInterface = sequelize.getQueryInterface();
+const { User } = require('../../models');
 
 const addTimezone = async () => {
     try {
         await sequelize.sync({ alter: true })
-        
-        await queryInterface.addColumn('User', 'timezone', { type: 
-            DataTypes.STRING,
+
+        await queryInterface.addColumn('User', 'timezone', {
+            type:
+                DataTypes.STRING,
             defaultValue: 'America/Los_Angeles',
         });
+        
+        const userData = await User.findAll();
+        const users = userData.map(user => user.get({ plain: true }));
 
-    } catch(error) {
+        console.log(users);
+
+    } catch (error) {
         console.log(error);
     }
 
