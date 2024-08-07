bbf91efb8313cf1d0241cbae87b668e0124dc96d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-08-06 17:25:06 -0700

    migration edits

--- server/seeds/migrations/addOrderCol.js
+++ server/seeds/migrations/addOrderCol.js
@@ -5,18 +5,18 @@ const { Notes } = require('../../models');
 
 const addOrderCol = async () => {
     try {
-        // await sequelize.sync({ alter: true })
+        await sequelize.sync({ alter: true })
 
         // await queryInterface.addColumn('notes', 'order', {
         //     type:
         //         DataTypes.INTEGER,
         // });
         
-        const notesData = await Notes.findAll();
-        const notes = notesData.map(note => note.get({ plain: true }));
+        // const notesData = await Notes.findAll();
+        // const notes = notesData.map(note => note.get({ plain: true }));
 
-        console.log(notes);
-        console.table(notes);
+        // console.log(notes);
+        // console.table(notes);
 
     } catch (error) {
         console.log(error);
