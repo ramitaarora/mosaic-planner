baa415b8d2082857e842f2e6165893823b314b82
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-09-27 17:16:09 -0700

    added set timeout to error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,11 +1,15 @@
-import Navigation from '../components/Navigation';
 import { css } from '@emotion/css'
+import { useEffect } from 'react';
 
 export default function Error() {
+    useEffect(() => {
+        setTimeout(() => {
+            window.location.replace('/login')
+        }, [1000])
+    }, [])
     return (
-        <div>
-            <Navigation />
-            <h1>Error</h1>
+        <div className={css`text-align: center; color: var(--line-background); margin: 20px auto;`}>
+            <h1>Error! Heading back home...</h1>
         </div>
         
     )
