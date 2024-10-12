7eb319ce6226c77b70aeab97c4da54283ed24368
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-10-11 18:05:07 -0700

    added login-demo route for demo dashboard

--- server/controllers/api/userRoutes.js
+++ server/controllers/api/userRoutes.js
@@ -94,6 +94,34 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/login-demo', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: 'demo@example.com' } });
+
+    if (!userData) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword('password12345');
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user: userData, loggedIn: true });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
